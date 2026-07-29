e54074ffc93b312dda42c1b362a62813fd9f87ff
Rehan <rehanquraishi037@gmail.com>
2026-07-30 01:27:50 +0530

    Day 1: Project initialized

---
+++ READE.md
@@ -0,0 +1,9 @@
+# AI Learnig Companion
+
+This is my AI/ML Learning project.
+
+Goal:
+- Lean AI by building real projects.
+- Build a portfolio for Tier-1 companies.
+
+Author: Rehan
---
+++ app.py
@@ -0,0 +1,8 @@
+print("=" * 40)
+print("        AI Learning Companion")
+print("=" * 40)
+
+name = input("Enter your name: ")
+
+print(f"Welcome, {name}")
+print("Let's build an amazing AI project.")
