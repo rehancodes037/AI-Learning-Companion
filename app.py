88002bd1b0ce6861e106fc2316cad0e82623f761
Rehan <rehanquraishi037@gmail.com>
2026-08-18 23:14:14 +0530

    Add notes list feature

--- app.py
+++ app.py
@@ -1,8 +1,62 @@
-print("=" * 40)
-print("        AI Learning Companion")
-print("=" * 40)
+import os
 
-name = input("Enter your name: ")
+print("AI Learning Companion")
 
-print(f"Welcome, {name}")
-print("Let's build an amazing AI project.")
+def save_notes():
+    topic = input("Enter topic: ")
+    notes = input("Enter notes: ")
+    filename = "notes/" + topic.replace(" ", "_").lower( ) + ".text"
+    file = open(filename, "w")
+    file.write(notes)
+    file.close()
+
+    print("Notes saved successfully!")
+
+ 
+
+def read_notes():
+    topic = input("Enter topic: ")
+    filename = "notes/" + topic.replace(" ", "_").lower()  + ".text"
+    try:
+     file = open(filename, "r")
+     content = file.read()
+     file.close()
+
+     print("\nYour Notes:")
+     print(content)
+    except FileNotFoundError:
+        print("No notes found for this topic.")
+
+def list_notes():
+  print("\nAvailable Notes:")
+
+  files = os.listdir("notes")
+  if not files:
+    print("No notes available")
+    return
+  for file in files:
+    
+   if file.endswith(".text"):
+     topic = file.replace(".text", "").replace("_", " ").title()
+     print("-", topic)
+while True:
+  print("\nAI Learning Companion")
+  print("1. Save Notes")
+  print("2. Read Notes")
+  print("3. List ALL Notes")
+  print("4. Exit")     
+
+  choice = input("Choose an option: ")
+
+  if choice == "1":
+    save_notes()
+  elif choice == "2":
+    read_notes()
+  elif choice == "3":
+    list_notes()  
+  elif choice =="4":
+    print("Goodbye!")
+    break
+            
+  else :
+    print("Invalid option")    
